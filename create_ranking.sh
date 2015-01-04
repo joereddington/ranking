b6bd89715b7fe2f1d36828cdaa108b579a088c54
Joseph Reddington <joe@joereddington.com>
2015-01-04 15:54:44 +0000

    moving forward, currently have a bug with numerical urls

--- create_ranking.sh
+++ create_ranking.sh
@@ -1,17 +1,20 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-DESTINATION_PAGE=/home/joereddington/joereddington.com/rankingProd.html
+DESTINATION_PAGE=/home/joereddington/joereddington.com/rankingProdTest.html
 cd $DIR
-rm $DIR/sources/*
-cd sources
+wget http://s3.amazonaws.com/alexa-static/top-1m.csv.zip
+unzip top-1m.csv.zip
+rm list1.txt
 while read -r url; do 
- wget -O "${url##http://}" "http://www.alexa.com/siteinfo/$url"
- sleep 10
-done < ../list_of_sources.txt
+ grep -m 1 "$url" top-1m.csv >> list1.txt
+ echo grep "$url" top-1m.csv
+done < list_of_sources.txt
 
 cat $DIR/start.html > $DESTINATION_PAGE 
-grep -o  "<img.*Global rank.*" * | sed -e 's/<[^>]*>//g' |  sed 's/\(^.*\):\(.*\)/<\/td><td><a href=\"http:\/\/\1\">\1<\/a><\/td><td> \2 <\/td><td><\/td><\/tr>/' > thelist.txt 
-cat thelist.txt | sort -n -k 3 | head -n 60 > thelist2.txt
-cat thelist2.txt | nl | sed 's/^/<tr><td>/g' >> $DESTINATION_PAGE
+cat list1.txt | sort -n | head -n 60 |  sed 's/\(^[0-9]*\),\(.*\)/<\/td><td><a href=\"http:\/\/\2\">\2<\/a><\/td><td> \1 <\/td><td><\/td><\/tr>/' > list2.txt 
+cat list2.txt | nl | sed 's/^/<tr><td>/g' >> $DESTINATION_PAGE
 echo "</table><br> Ranking last updated at: " >> $DESTINATION_PAGE
 date >> $DESTINATION_PAGE
 cat $DIR/end.html >> $DESTINATION_PAGE
+#rm top-1m.*
+#rm list1.txt
+#rm list2.txt
