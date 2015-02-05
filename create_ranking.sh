2ae817f9fc4e0bc77ca9c4b44eff408d3fe11e42
Joseph Reddington <joe@joereddington.com>
2015-02-05 20:11:36 +0000

    Rewritten the code so that it produces just a simple bit of html for import.  I used the snippet at http://www.wprecipes.com/shortcode-to-display-external-files-on-your-posts to import these properly into wordpress, demo at http://joereddington.com/rankingpro/

--- create_ranking.sh
+++ create_ranking.sh
@@ -1,5 +1,5 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-DESTINATION_PAGE=/home/joereddington/joereddington.com/rankingProd.html
+DESTINATION_PAGE=/home/joereddington/joereddington.com/rankingPro.txt
 cd $DIR
 wget http://s3.amazonaws.com/alexa-static/top-1m.csv.zip
 unzip top-1m.csv.zip
@@ -7,12 +7,10 @@ rm list1.txt
 while read -r url; do 
  grep -m 1 "$url" top-1m.csv >> list1.txt
 done < list_of_sources.txt
-cat $DIR/start.html > $DESTINATION_PAGE 
-cat list1.txt |  sed 's/\(^[0-9]*\),\(.*\)/<\/td><td><a href=\"http:\/\/\2\">\2<\/a><\/td><td> \1 <\/td><td><\/td><\/tr>/' cat list1.txt | sort -n -k 3| head -n 60 > list2.txt 
-cat list2.txt | nl | sed 's/^/<tr><td>/g' >> $DESTINATION_PAGE
+cat list1.txt |  sed 's/\(^[0-9]*\),\(.*\)/<\/td><td><a href=\"http:\/\/\2\">\2<\/a><\/td><td> \1 <\/td><td><\/td><\/tr>/'  | sort -n -k 3| head -n 60 > list2.txt 
+cat list2.txt | nl | sed 's/^/<tr><td>/g' > $DESTINATION_PAGE
 echo "</table><br> Ranking last updated at: " >> $DESTINATION_PAGE
 date >> $DESTINATION_PAGE
-cat $DIR/end.html >> $DESTINATION_PAGE
 rm top-1m.*
 rm list1.txt
 rm list2.txt
