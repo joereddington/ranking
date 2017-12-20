f9071fb1d9720cca169690e9258b4413ed2595b3
Joseph Reddington <joe@joereddington.com>
2017-12-20 18:03:22 +0000

    snapshot

--- create_ranking.sh
+++ create_ranking.sh
@@ -7,7 +7,7 @@ rm list1.txt
 while read -r url; do 
  grep -m 1 "$url" top-1m.csv >> list1.txt
 done < list_of_sources.txt
-cat list1.txt |  sed 's/\(^[0-9]*\),\(.*\)/<\/td><td><a href=\"http:\/\/\2\">\2<\/a><\/td><td> \1 <\/td><td><\/td><\/tr>/'  | sort -n -k 3| head -n 60 > list2.txt 
+cat list1.txt |  sed 's/\(^[0-9]*\),\(.*\)/<\/td><td><a href=\"http:\/\/\2\">\2<\/a><\/td><td> \1 <\/td><td><\/td><\/tr>/'  | sort -n -k 3| head -n 200 > list2.txt 
 cat list2.txt | nl | sed 's/^/<tr><td>/g' > $DESTINATION_PAGE
 echo "</table><br> Ranking last updated at: " >> $DESTINATION_PAGE
 date >> $DESTINATION_PAGE
